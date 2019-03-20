10b899b758ce11077846874c0eabadc38cdfa2fa
Håvard Anda Estensen <haavard.ae@gmail.com>
2019-03-20 23:30:00 +0100

    Add image uploader

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	setupRoutes()
+}
+
+func setupRoutes() {
+	http.HandleFunc("/upload", uploadImage)
+	http.ListenAndServe("localhost:8080", nil)
+	fmt.Println("Running on port 8080")
+}
+
+func uploadImage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Uploading Image")
+
+	r.ParseMultipartForm(10 << 20)
+	file, _, err := r.FormFile("selectedImage")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+
+	tempFile, err := ioutil.TempFile("temp-images", "upload-*.png")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer tempFile.Close()
+
+	fileBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	tempFile.Write(fileBytes)
+	fmt.Fprintf(w, "Successfully Uploaded File\n")
+}
